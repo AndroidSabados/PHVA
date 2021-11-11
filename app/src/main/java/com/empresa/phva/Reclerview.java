5cadca27d9de843cd63bfe9185e7da9f7580ba29
SantiagoHincapie <48575772+SantiagoHincapie@users.noreply.github.com>
2021-11-10 20:42:14 -0500

    creacion activity_reclerview

---
+++ app/src/main/java/com/empresa/phva/Reclerview.java
@@ -0,0 +1,14 @@
+package com.empresa.phva;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reclerview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reclerview);
+    }
+}
