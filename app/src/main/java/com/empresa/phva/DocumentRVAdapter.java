a39378d51a61530e1daad60541ca46e1abac0460
fedegomez78 <fedebgomez@gmail.com>
2021-11-11 18:23:11 -0500

    colores en el onBindView

--- app/src/main/java/com/empresa/phva/DocumentRVAdapter.java
+++ app/src/main/java/com/empresa/phva/DocumentRVAdapter.java
@@ -3,9 +3,12 @@ package com.empresa.phva;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,6 +40,9 @@ public class DocumentRVAdapter extends RecyclerView.Adapter<DocumentRVAdapter.Vi
         // on below line we are inflating our layout
         // file for our recycler view items.
         view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_document_item, parent, false);
+
+
+
         return new ViewHolder(view);
     }
 
@@ -49,6 +55,18 @@ public class DocumentRVAdapter extends RecyclerView.Adapter<DocumentRVAdapter.Vi
         holder.tvDocumentUrl.setText(modal.getUrl());
         holder.tvDocumentDescription.setText(modal.getDescription());
         holder.tvDocumentStatus.setText(modal.getStatus());
+
+        Log.e("ErrorFG", "onBindViewHolder: " + holder.tvDocumentType.getText().toString() );
+
+        if (holder.tvDocumentType.getText().toString().equals("Tipo1")  ){
+            Toast.makeText(context, "OJO: "+holder.tvDocumentType.getText().toString(), Toast.LENGTH_SHORT).show();
+            holder.lyPpal.setBackgroundColor(Color.GREEN);
+        }else{
+            holder.lyPpal.setBackgroundColor(Color.RED);
+        }
+
+
+
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,6 +87,7 @@ public class DocumentRVAdapter extends RecyclerView.Adapter<DocumentRVAdapter.Vi
 
         // creating variables for our text views.
         private TextView tvDocumentType, tvDocumentUrl, tvDocumentDescription, tvDocumentStatus;
+        private LinearLayout lyPpal;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -77,6 +96,10 @@ public class DocumentRVAdapter extends RecyclerView.Adapter<DocumentRVAdapter.Vi
             tvDocumentUrl = itemView.findViewById(R.id.tvDocumentUrl);
             tvDocumentDescription = itemView.findViewById(R.id.tvDocumentDescription);
             tvDocumentStatus = itemView.findViewById(R.id.tvDocumentStatus);
+
+            lyPpal = itemView.findViewById(R.id.lyPpal);
+
+
         }
     }
 }
