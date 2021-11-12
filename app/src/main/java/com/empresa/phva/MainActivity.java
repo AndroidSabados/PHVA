9c4b84d7284972047bcb12b5015caf07ccc00608
Camila Grajales <maria.grajales9100@unaula.edu.co>
2021-11-12 18:51:00 -0500

    Pantalla de registro

--- app/src/main/java/com/empresa/phva/MainActivity.java
+++ app/src/main/java/com/empresa/phva/MainActivity.java
@@ -2,13 +2,29 @@ package com.empresa.phva;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        }
+    public void pRegistro (View view){
+        Intent registrarse = new Intent(this, Registro.class);
+        startActivity(registrarse);
     }
-}
+
+    public void pListadoAlertas (View view){
+        Intent listado = new Intent(this, ListadoAlertas.class);
+        startActivity(listado);
+
+
+}}
