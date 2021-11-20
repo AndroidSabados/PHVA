7eada727298d17609fd16688a1c6d24c5abb16f1
Camila Grajales <maria.grajales9100@unaula.edu.co>
2021-11-19 19:35:47 -0500

    *Cambio de encabezado * Adición de código para cambio de tema (Claro/oscuro)

--- app/src/main/java/com/empresa/phva/MainActivity.java
+++ app/src/main/java/com/empresa/phva/MainActivity.java
@@ -1,20 +1,28 @@
 package com.empresa.phva;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Switch swi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        swi= (Switch) findViewById(R.id.tema);
+        ClaroOscuro();
 
         }
     public void pRegistro (View view){
@@ -27,4 +35,50 @@ public class MainActivity extends AppCompatActivity {
         startActivity(listado);
 
 
-}}
+}
+
+    public void ClaroOscuro(){
+        SharedPreferences sp = getSharedPreferences("SP", this.MODE_PRIVATE);
+        int theme = sp.getInt("Theme",1);
+        if (theme==0){
+            //getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            swi.setChecked(true);
+        }
+        else{
+            //getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            swi.setChecked(false);
+        }
+    }
+
+     public void onClick(View view){
+         SharedPreferences sp = getSharedPreferences("SP", this.MODE_PRIVATE);
+         SharedPreferences.Editor editor = sp.edit();
+
+         if (view.getId()==R.id.tema){
+            if (swi.isChecked()){
+               editor.putInt("Theme",0);
+            } else{
+                editor.putInt("Theme",1);
+            }
+            editor.commit();
+            ClaroOscuro();
+        }
+     }
+
+
+
+    /*swi.(new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            if (swi.isChecked()) {
+                ClaroOscuro(0);
+            } else {
+                ClaroOscuro(1);
+            }
+        }
+    });*/
+
+
+}
